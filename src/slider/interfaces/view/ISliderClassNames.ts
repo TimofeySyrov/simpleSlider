a56bdd04dee52c6352f217cea93bd72a1a1e5792
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-08-20 12:59:58 +0300

    added interface for slider class names

---
+++ src/slider/interfaces/view/ISliderClassNames.ts
@@ -0,0 +1,12 @@
+interface ISliderClassNames {
+  slider: string,
+  bar: string,
+  barScale: string,
+  ruler: string,
+  rulerItem: string,
+  toggle: string,
+  toggleHandle: string,
+  thumb: string
+}
+
+export default ISliderClassNames
