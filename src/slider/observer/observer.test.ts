c93809a00ba64c1376009a1b42be59333dd149f8
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-08-24 20:31:08 +0300

    добавление unit-теста для Observer

---
+++ src/slider/observer/observer.test.ts
@@ -0,0 +1,25 @@
+import Observer from "./Observer";
+
+describe('Observable:', () => {
+  const observable = new Observer();
+  const data = { text: 'some data' }
+
+  test('should add subscriber to observers and send them data', () => {
+    const subscriber = jest.fn();
+
+    observable.subscribe(subscriber);
+    observable.notify(data);
+
+    expect(subscriber).toHaveBeenCalledWith(data);
+  });
+
+  test('should delete subscriber from observers', () => {
+    const subscriber = jest.fn();
+
+    observable.subscribe(subscriber);
+    observable.unsubscribe(subscriber);
+    observable.notify(data);
+
+    expect(subscriber).not.toHaveBeenCalledWith();
+  });
+})
