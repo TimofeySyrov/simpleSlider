9f7b0c51cd9e0e6e70d7c41bf8e9571c55f3d6f5
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-08-25 16:52:55 +0300

    Добавление обертки приложения в jquery плагин

--- src/slider/index.ts
+++ src/slider/index.ts
@@ -1,1 +1,15 @@
-import './some.scss'
+/// <reference path="./interfaces/IJQuerySlider.d.ts" />
+
+import IModelOptions from "./interfaces/IModelOptions";
+import SimpleSlider from "./simpleSlider";
+
+(function ( $ ) {
+  $.fn.simpleSlider = function (options: IModelOptions) {
+
+    const domParent:HTMLDivElement = this[0];
+
+    const simpleSlider = new SimpleSlider(domParent, options)
+
+    return simpleSlider
+  }
+}(jQuery));
