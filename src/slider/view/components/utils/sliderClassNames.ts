97c2ab720b8aad142ddf8935f2f2199c0e78712d
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-08-25 16:39:30 +0300

    Добавление утилит для отображения слайдера

---
+++ src/slider/view/components/utils/sliderClassNames.ts
@@ -0,0 +1,14 @@
+import ISliderClassNames from '../../../interfaces/view/ISliderClassNames'
+
+const sliderClassNames: ISliderClassNames = {
+  slider: 'simpleSlider',
+  bar: 'bar',
+  barScale: 'bar__scale',
+  toggle: 'toggle',
+  toggleHandle: 'toggle__handle',
+  thumb: 'thumb',
+  ruler: 'ruler',
+  rulerItem: 'ruler__item',
+};
+
+export default sliderClassNames
