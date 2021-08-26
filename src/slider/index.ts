1dc4f69f816af0a2f79c74068d378cb753b10afe
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-08-26 19:36:58 +0300

    Добавлено слияние базовых настроек модели с пользовательскими

--- src/slider/index.ts
+++ src/slider/index.ts
@@ -1,15 +1,17 @@
 /// <reference path="./interfaces/IJQuerySlider.d.ts" />
 
 import IModelOptions from "./interfaces/IModelOptions";
+import defaultModelOptions from "./model/utils/defaultModelOptions";
 import SimpleSlider from "./simpleSlider";
 
 (function ( $ ) {
   $.fn.simpleSlider = function (options: IModelOptions) {
 
     const domParent:HTMLDivElement = this[0];
+    const finalOptions = $.extend(defaultModelOptions, options);
 
-    const simpleSlider = new SimpleSlider(domParent, options)
+    const simpleSlider = new SimpleSlider(domParent, finalOptions)
 
-    return simpleSlider
+    return simpleSlider;
   }
 }(jQuery));
