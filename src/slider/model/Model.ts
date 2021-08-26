79dfe1b724f8bc2d59519e78f86922114df38824
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-08-26 19:34:33 +0300

    Добавлена проверка настроек модели

--- src/slider/model/Model.ts
+++ src/slider/model/Model.ts
@@ -1,7 +1,6 @@
 import IModelOptions from "../interfaces/IModelOptions";
 import Observer from "../observer/Observer";
 
-
 class Model extends Observer {
   
   private modelOptions: IModelOptions;
@@ -22,9 +21,58 @@ class Model extends Observer {
   }
 
   private checkModelOptions (checkModelOptions: IModelOptions): IModelOptions {
-    const checkedModelOptions = checkModelOptions;
+    let confirmedOptions = checkModelOptions;
+    const { min, max, currentValue, step } = confirmedOptions;
 
-    return checkedModelOptions;
+    /* Если минимальное значение слайдера больше максимального, то 
+    минимальному значению присваивается максимальное */
+    if(min > max) {
+      confirmedOptions.min = max;
+    }
+
+    /* Шаг не может быть отрицательным числом или равным нулю.
+    Если шаг меньше нуля или равен ему, то шагу присваивается 
+    максимально допустимый шаг, относительно значений слайдера */
+    if(step <= 0) { 
+      confirmedOptions.step = max - min;
+    }
+
+    /* Если шаг больше максимального значения слайдера, то шагу 
+    присваивается максимально допустимый шаг, относительно значений слайдера */
+    if(step > max) {
+      confirmedOptions.step = max - min;
+    }
+
+    switch (typeof currentValue) {
+
+      case 'number':
+        if(currentValue > max) { 
+          confirmedOptions.currentValue = max;
+        }
+        if(currentValue < min) {
+          confirmedOptions.currentValue = min;
+        }
+        break;
+
+      case 'object':
+        if(currentValue.min > currentValue.max) {
+          confirmedOptions.currentValue = { min: currentValue.max, max: currentValue.max }
+        }
+        
+        break;
+
+      case 'string':
+        if(currentValue == 'middle') {
+          confirmedOptions.currentValue = (max - min) / 2;
+        }
+        break;
+
+      default:
+        confirmedOptions.currentValue = (max - min) / 2;
+        break;
+    }
+
+    return confirmedOptions;
   }
 }
 
