4905823180e12c0fab1c68f888ebe8fa2a4ab614
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-08-26 19:32:58 +0300

    Добавлен базовый конфиг модели

---
+++ src/slider/model/utils/defaultModelOptions.ts
@@ -0,0 +1,13 @@
+import IModelOptions from "../../interfaces/IModelOptions";
+
+const defaultModelOptions: IModelOptions = {
+  min: 0,
+  max: 100,
+  step: 1,
+  orientation: 'horizontal',
+  currentValue: 1,
+  withThumb: true,
+  withRuler: true
+}
+
+export default defaultModelOptions;
