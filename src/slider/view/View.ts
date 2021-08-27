8729e03441e90333e00a77e11e53aeef7d2c3c41
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-08-27 18:06:33 +0300

    Добавлен базовый функционал одиночного слайдера при клике

--- src/slider/view/View.ts
+++ src/slider/view/View.ts
@@ -48,7 +48,16 @@ class View extends Observer {
     }
   }
 
+  private getToggles () {
+    const { currentValue, withThumb } = this.modelOptions;
+
+    if(typeof currentValue === 'object') {
+
+    }
+  }
+
   private createDOMSlider () {
+    const { orientation, withDoubleHandle } = this.modelOptions;
     const templateHtmlSlider = document.createElement('div');
     templateHtmlSlider.classList.add(`${sliderClassNames.slider}`);
 
@@ -62,9 +71,98 @@ class View extends Observer {
     if(this.ruler) {
       templateHtmlSlider.appendChild(this.ruler.getHtml());
     }
-    
+
+    if(orientation === 'vertical') {
+      this.bar.getDom().classList.add(`${sliderClassNames.barVertical}`);
+      this.toggle.getDom().classList.add(`${sliderClassNames.toggleVertical}`);
+      this.thumb.getDom().classList.add(`${sliderClassNames.thumbVertical}`);
+    }
+
     this.domParent.appendChild(templateHtmlSlider);
+    this.bar.getDom().addEventListener("click", this.setClickPosition.bind(this));
   }
+
+  private getCurrentValuePosition (currentValue: number): number {
+    const { min, max } = this.modelOptions;
+    const percentPosition = (currentValue - min) / (max - min);
+    return percentPosition;
+  }
+
+  private getBarLength (): number {
+    const { orientation } = this.modelOptions;
+
+    if(orientation == 'horizontal') {
+      return this.bar.getDom().offsetWidth;
+    }
+    if(orientation == 'vertical') {
+      return this.bar.getDom().offsetHeight;
+    }
+  }
+
+  private getBarPositionStart (): number {
+    const { orientation } = this.modelOptions;
+
+    if(orientation == 'horizontal') {
+      return this.bar.getDom().getBoundingClientRect().x;
+    }
+    if(orientation == 'vertical') {
+      return this.bar.getDom().getBoundingClientRect().y;
+    }
+  }
+
+  private getClickCurrentValue (percent: number) {
+    const { min, max, step } = this.modelOptions;
+    const currentValue = step * Math.round(percent * (max - min) / step) + min;
+
+    return currentValue;
+  }
+
+  private getClickPercentPosition (event: MouseEvent): number {
+    const { orientation } = this.modelOptions;
+    const barLength = this.getBarLength();
+    const barPositionStart = this.getBarPositionStart();
+    const clickPosition = orientation == 'horizontal' ? event.pageX : event.pageY;
+    const pixelPosition = clickPosition - barPositionStart;
+    const percentPosition = pixelPosition / barLength;
+
+    return percentPosition;
+  }
+
+  private setScalePercentPosition (percent: number) {
+    const { orientation } = this.modelOptions; 
+    const scale = this.bar.getDom().querySelector(`.${sliderClassNames.barScale}`);
+
+    if(orientation === 'horizontal') {
+      scale.setAttribute("style", `transform: scale(${percent}, 1);`);
+    }
+    if(orientation === 'vertical') {
+      scale.setAttribute("style", `transform: scale(1, ${percent});`);
+    }
+    
+  }
+
+  private setTogglePercentPosition (percent: number) {
+    const { orientation } = this.modelOptions;
+    const transformOrientation = orientation == 'horizontal' ? 'X' : 'Y';
+    const transformPercent = percent * 1000;
+    const toggle = this.toggle.getDom();
+    
+    toggle.setAttribute("style", `transform: translate${transformOrientation}(${transformPercent}%);`);
+  }
+
+  private setThumbCurrentValue (currentValue: number) {
+    const thumb = this.thumb.getDom().innerHTML = `${currentValue}`;
+  }
+
+  private setClickPosition (event: MouseEvent) {
+    const percentPosition = this.getClickPercentPosition(event);
+    const currentValue = this.getClickCurrentValue(percentPosition);
+
+    this.setTogglePercentPosition(percentPosition);
+    this.setScalePercentPosition(percentPosition);
+    this.setThumbCurrentValue(currentValue);
+  }
+  
 }
 
 export default View;
