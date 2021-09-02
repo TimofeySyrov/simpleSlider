ea9357717959a27ef34f117926cab032da18a1d9
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-09-03 00:13:58 +0300

    Добавление подключей классов

--- src/slider/view/components/utils/sliderClassNames.ts
+++ src/slider/view/components/utils/sliderClassNames.ts
@@ -2,15 +2,31 @@ import ISliderClassNames from '../../../interfaces/view/ISliderClassNames'
 
 const sliderClassNames: ISliderClassNames = {
   slider: 'simpleSlider',
-  bar: 'simpleSlider__bar',
-  range: 'simpleSlider__range',
-  toggle: 'simpleSlider__toggle',
-  thumb: 'simpleSlider__thumb',
-  scale: 'simpleSlider__scale',
-  barVertical: 'simpleSlider__bar_vertical',
-  rangeVertical: 'simpleSlider__range_vertical',
-  toggleVertical: 'simpleSlider__toggle_vertical',
-  thumbVertical: 'simpleSlider__thumb_vertical'
+  bar: {
+    main: 'simpleSlider__bar',
+    horizontal: 'simpleSlider__bar_horizontal',
+    vertical: 'simpleSlider__bar_vertical'
+  },
+  range: {
+    main: 'simpleSlider__range',
+    horizontal: 'simpleSlider__range_horizontal',
+    vertical: 'simpleSlider__range_vertical'
+  },
+  toggle: {
+    main: 'simpleSlider__toggle',
+    horizontal: 'simpleSlider__toggle_horizontal',
+    vertical: 'simpleSlider__toggle_vertical'
+  },
+  thumb: {
+    main: 'simpleSlider__thumb',
+    horizontal: 'simpleSlider__thumb_horizontal',
+    vertical: 'simpleSlider__thumb_vertical'
+  },
+  scale: {
+    main: 'simpleSlider__scale',
+    horizontal: 'simpleSlider__scale_horizontal',
+    vertical: 'simpleSlider__scale_vertical'
+  }
 };
 
 export default sliderClassNames
