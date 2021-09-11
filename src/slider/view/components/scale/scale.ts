e7e2b10f595202a6d170e3d96a41444af300f866
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-09-12 00:40:38 +0300

    Добавлены методы добавления и получения значений шкалы

--- src/slider/view/components/scale/scale.ts
+++ src/slider/view/components/scale/scale.ts
@@ -1,24 +1,41 @@
 import sliderClassNames from '../utils/sliderClassNames'
 
 class Scale {
-  private dom: HTMLDivElement;
-  constructor () {
+  
+  private dom: HTMLElement;
+
+  constructor() {
 
     this.createDom();
   }
 
   private createDom () {
-    const domScale = document.createElement('div');
+    const domScale = document.createElement('ul');
     domScale.classList.add(`${sliderClassNames.scale.main}`);
 
     this.saveDom(domScale);
   }
 
-  public saveDom (el: HTMLDivElement) {
+  public addItem (value: number): HTMLElement {
+    const domScaleItem = document.createElement('li');
+    domScaleItem.classList.add(`${sliderClassNames.scaleItem.main}`);
+
+    domScaleItem.setAttribute(`data-value`, `${value}`);
+    domScaleItem.innerHTML = `${value}`;
+    this.dom.appendChild(domScaleItem);
+    return domScaleItem;
+  }
+
+  public getItems (): NodeListOf<HTMLElement> {
+    const list = this.dom.querySelectorAll(`.${sliderClassNames.scaleItem.main}`);
+    return list as NodeListOf<HTMLElement>;
+  }
+
+  public saveDom (el: HTMLElement) {
     this.dom = el;
   }
 
-  public getDom (): HTMLDivElement {
+  public getDom (): HTMLElement {
     return this.dom;
   }
 }
