152adfe89189e01b698f1b6a14d21c670cf8a100
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-09-16 23:41:37 +0300

    Реализован callback для onSlide

--- src/slider/simpleSlider.ts
+++ src/slider/simpleSlider.ts
@@ -1,19 +1,27 @@
 import IModelOptions from "./interfaces/IModelOptions";
 import Controller from "./controller/Controller";
+import './view/styles/view.scss';
+import { TDomParent } from "./interfaces/namespace";
 
 class SimpleSlider {
 
   private options: IModelOptions;
-  private domParent: HTMLDivElement;
+  private domParent: TDomParent;
+  private controller: Controller;
 
-  constructor (domParent: HTMLDivElement, options: IModelOptions) {
+  constructor (domParent: TDomParent, options: IModelOptions) {
     this.domParent = domParent
     this.options = options;
-    this.initApp();
+    this.controller = new Controller(this.domParent, this.options);
+    this.initUserCallbackEvents();
   }
 
-  private initApp () {
-    const controller = new Controller(this.domParent, this.options)
+  private initUserCallbackEvents() {
+    const onSlide = this.options.onSlide;
+
+    if(onSlide) {
+      this.controller.events.currentValueChanged.subscribe(onSlide)
+    }
   }
   
 }
