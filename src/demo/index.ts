4930d128244e2382b410df155df89413564b5d28
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-09-19 23:11:04 +0300

    Добавление верстки демо-страницы

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,92 +1,51 @@
 import './index.scss';
+import '../slider/index';
+import '../slider/view/styles/view.scss';
+import './panel/index';
+import './panel/styles/view.scss';
 
-var slider = document.querySelector('.simpleSlider');
+/* Slider boxes */
+const sliderBoxes_1 = {
+  sliderBox: $('.js-slider__slider-box-1'),
+  panelBox: $('.js-slider__panel-box-1')
+};
 
-var bar = slider.querySelector('.bar');
-var scale = bar.querySelector('.bar__scale');
+const sliderBoxes_2 = {
+  sliderBox: $('.js-slider__slider-box-2'),
+  panelBox: $('.js-slider__panel-box-2')
+};
 
-var toggle = slider.querySelector('.toggle');
-var handle = toggle.querySelector('.toggle__handle');
-var thumb = toggle.querySelector('.thumb');
+const sliderBoxes_3 = {
+  sliderBox: $('.js-slider__slider-box-3'),
+  panelBox: $('.js-slider__panel-box-3')
+};
 
-var ruler = slider.querySelector('.ruler');
-var ruler__item = ruler.querySelector('.ruler__item');
-
-const maxValue = 10;
-const minValue = 5;
-const step = 2;
-
-function getRulerValues(): any[] {
-
-  const midQuantity = Math.ceil((maxValue - minValue) / step);
-  const viewStep = Math.ceil(midQuantity / 5) * step;
-  const midArr = [];
-  let value = minValue;
-
-  for (let i = 0; value < maxValue; i += 1) {
-    value += viewStep;
-    if (value < maxValue) {
-      midArr.push(value);
-    }
-  }
-  
-  return [minValue, ...midArr, maxValue]
-}
-
-function getRulerItemPosition(item:number) {
-  const posItem = ((item - minValue) / (maxValue - minValue)) * 320;
-  return posItem
-}
-
-function setRulerValues() {
-  const values = getRulerValues();
-  
-
-  values.map(function(item) {
-    const posX = getRulerItemPosition(item)
-    ruler.innerHTML += `<li class="ruler__item" style="transform: translateX(${posX}px);">${item}</li>`;
-  })
-}
-
-setRulerValues();
-
-thumb.innerHTML = `${minValue}`;
-
-function updateDisplay(event: MouseEvent) {
-  // ПОЛУЧАЕМ ЗНАЧЕНИЯ
-  var sliderWidth = bar.getBoundingClientRect().width; // длина слайдера
-  var windowXSliderStart = bar.getBoundingClientRect().x; // координата X начала слайдера относительно окна пользователя
-  var windowX = event.pageX; // координата X из места клика относительно окна пользователя
-
-  // ВЫЧИСЛЯЕМ КУДА КЛИКНУЛ
-  var sliderX = windowX - windowXSliderStart; // координата X из места клика относительно длины слайдера
-  var procent = sliderX / sliderWidth; // sliderX переводится в проценты для transform: scale
-
-  // ВЫЧИСЛЯЕМ currentValue относительно position
-  var currentValue = step * Math.round(procent * (maxValue - minValue) / step) + minValue;
-
-  if(procent >= 0 && procent <= 1){
-    // УСТАНАВЛИВАЕМ ДЛИНУ для scale
-    scale.setAttribute("style", `transform: scale(${procent}, 1);`);
-
-    // Переносим toggle в место клика
-    toggle.setAttribute("style", `transform: translateX(${sliderX}px);`);
-
-    // Передаем currentValue в thumb
-    thumb.innerHTML = `${currentValue}`;
-  }
-}
-
-slider.addEventListener('click', updateDisplay);
-
-bar.addEventListener('mousedown', e => {
-  document.addEventListener('mousemove', updateDisplay);
+/* Sliders */
+const slider_1 = sliderBoxes_1.sliderBox.simpleSlider();
+const slider_2 = sliderBoxes_2.sliderBox.simpleSlider({
+  min: -10,
+  max: 0,
+  orientation: 'vertical',
+  step: 0.01,
+  type: 'from-end',
+  withRange: false,
+  currentValue: -7
+});
+const slider_3 = sliderBoxes_3.sliderBox.simpleSlider({
+  min: -50,
+  max: 50,
+  type: 'range',
+  currentValue: {
+    min: -20,
+    max: 20
+  },
+  step: 10,
+  withRange: false,
+  withScale: false,
+  withThumb: false
 });
 
-toggle.addEventListener('mousedown', e => {
-  document.addEventListener('mousemove', updateDisplay);
-});
-
-document.addEventListener('mouseup', e => {
-  document.removeEventListener('mousemove', updateDisplay);
-});
+/* Slider panels */
+const panel_1 = sliderBoxes_1.panelBox.simpleSliderPanel(slider_1);
+const panel_2 = sliderBoxes_2.panelBox.simpleSliderPanel(slider_2);
+const panel_3 = sliderBoxes_3.panelBox.simpleSliderPanel(slider_3);
