acb71a0f2d60b6a1b20d1f33301e664c2cbc7da2
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-09-19 23:07:12 +0300

    Добавление интерфейса INodes для панели слайдера

---
+++ src/demo/panel/interfaces/INodes.ts
@@ -0,0 +1,17 @@
+interface INodes {
+  min: HTMLInputElement,
+  max: HTMLInputElement,
+  from: HTMLInputElement,
+  to: HTMLInputElement,
+  step: HTMLInputElement,
+  vertical: HTMLInputElement,
+  horizontal: HTMLInputElement,
+  'from-end': HTMLInputElement,
+  'from-start': HTMLInputElement,
+  'range': HTMLInputElement,
+  withRange: HTMLInputElement,
+  withThumb: HTMLInputElement,
+  withScale: HTMLInputElement
+}
+
+export default INodes;
