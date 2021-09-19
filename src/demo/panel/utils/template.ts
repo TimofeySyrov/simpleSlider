953547c193e4fb30b7c70c733ab2d8e26e0abc62
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-09-19 23:08:48 +0300

    Добавление html-шаблона для панели

---
+++ src/demo/panel/utils/template.ts
@@ -0,0 +1,86 @@
+const template = `
+  <div class="panel__container">
+    <div class="panel__item">
+      <div class="panel__item-title">Diapason:</div>
+      <div class="panel__form">
+        <label class="panel__label">
+          <span class="panel__label-title">min</span>
+          <input type="number" class="panel__input panel__min-input js-panel__min-input">
+        </label>
+        <label class="panel__label">
+          <span class="panel__label-title">max</span>
+          <input type="number" class="panel__input panel__max-input js-panel__max-input">
+        </label>
+      </div>
+    </div>
+    <div class="panel__item">
+      <div class="panel__item-title">Current value:</div>
+      <div class="panel__form">
+        <label class="panel__label">
+          <span class="panel__label-title">from</span>
+          <input type="number" class="panel__input panel__from-input js-panel__from-input">
+        </label>
+        <label class="panel__label">
+          <span class="panel__label-title">to</span>
+          <input type="number" class="panel__input panel__to-input js-panel__to-input">
+        </label>
+      </div>
+    </div>
+    <div class="panel__item">
+      <div class="panel__form">
+        <label class="panel__label panel__label_single">
+          <span class="panel__label-title">Step:</span>
+          <input type="number" class="panel__input panel__step-input js-panel__step-input">
+        </label>
+      </div>
+    </div>
+    <div class="panel__item">
+      <div class="panel__item-title">Orientation:</div>
+      <form class="panel__form panel__orient-form">
+        <label class="panel__label">
+          <input type="radio" name="orientation" class="panel__input panel__vertical-input js-panel__vertical-input">
+          <span class="panel__label-title">vertical</span>
+        </label>
+        <label class="panel__label">
+          <input type="radio" name="orientation" class="panel__input panel__horizontal-input js-panel__horizontal-input">
+          <span class="panel__label-title">horizontal</span>
+        </label>
+      </form>
+    </div>
+    <div class="panel__item">
+      <div class="panel__item-title">Type:</div>
+      <form class="panel__form panel__type-form">
+        <label class="panel__label">
+          <input type="radio" name="type" class="panel__input panel__from-start-input js-panel__from-start-input">
+          <span class="panel__label-title">from-start</span>
+        </label>
+        <label class="panel__label">
+          <input type="radio" name="type" class="panel__input panel__from-end-input js-panel__from-end-input">
+          <span class="panel__label-title">from-end</span>
+        </label>
+        <label class="panel__label">
+          <input type="radio" name="type" class="panel__input panel__range-input js-panel__range-input">
+          <span class="panel__label-title">range</span>
+        </label>
+      </form>
+    </div>
+    <div class="panel__item">
+      <div class="panel__item-title">Other settings:</div>
+      <div class="panel__form">
+        <label class="panel__label">
+          <input type="checkbox" class="panel__input panel__range-input js-panel__withRange-input">
+          <span class="panel__label-title">withRange</span>
+        </label>
+        <label class="panel__label">
+          <input type="checkbox" class="panel__input panel__thumb-input js-panel__withThumb-input">
+          <span class="panel__label-title">withThumb</span>
+        </label>
+        <label class="panel__label">
+          <input type="checkbox" class="panel__input panel__scale-input js-panel__withScale-input">
+          <span class="panel__label-title">withScale</span>
+        </label>
+      </div>
+    </div>
+  </div>`;
+
+export default template;
