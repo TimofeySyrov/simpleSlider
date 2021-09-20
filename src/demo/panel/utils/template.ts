5b44fe2c61729787c003301ff87ced62aa5473d7
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-09-20 23:58:36 +0300

    Рефакторинг template panel

--- src/demo/panel/utils/template.ts
+++ src/demo/panel/utils/template.ts
@@ -5,11 +5,11 @@ const template = `
       <div class="panel__form">
         <label class="panel__label">
           <span class="panel__label-title">min</span>
-          <input type="number" class="panel__input panel__min-input js-panel__min-input">
+          <input type="number" data-option-type="min" class="panel__input panel__min-input js-panel__min-input">
         </label>
         <label class="panel__label">
           <span class="panel__label-title">max</span>
-          <input type="number" class="panel__input panel__max-input js-panel__max-input">
+          <input type="number" data-option-type="max" class="panel__input panel__max-input js-panel__max-input">
         </label>
       </div>
     </div>
@@ -18,11 +18,11 @@ const template = `
       <div class="panel__form">
         <label class="panel__label">
           <span class="panel__label-title">from</span>
-          <input type="number" class="panel__input panel__from-input js-panel__from-input">
+          <input type="number" data-option-type="from" class="panel__input panel__from-input js-panel__from-input">
         </label>
         <label class="panel__label">
           <span class="panel__label-title">to</span>
-          <input type="number" class="panel__input panel__to-input js-panel__to-input">
+          <input type="number" data-option-type="to" class="panel__input panel__to-input js-panel__to-input">
         </label>
       </div>
     </div>
@@ -30,7 +30,7 @@ const template = `
       <div class="panel__form">
         <label class="panel__label panel__label_single">
           <span class="panel__label-title">Step:</span>
-          <input type="number" class="panel__input panel__step-input js-panel__step-input">
+          <input type="number" data-option-type="step" class="panel__input panel__step-input js-panel__step-input">
         </label>
       </div>
     </div>
@@ -38,11 +38,11 @@ const template = `
       <div class="panel__item-title">Orientation:</div>
       <form class="panel__form panel__orient-form">
         <label class="panel__label">
-          <input type="radio" name="orientation" class="panel__input panel__vertical-input js-panel__vertical-input">
+          <input type="radio" name="orientation" data-option-type="orientation" value="vertical" class="panel__input panel__vertical-input js-panel__vertical-input">
           <span class="panel__label-title">vertical</span>
         </label>
         <label class="panel__label">
-          <input type="radio" name="orientation" class="panel__input panel__horizontal-input js-panel__horizontal-input">
+          <input type="radio" name="orientation" data-option-type="orientation" value="horizontal" class="panel__input panel__horizontal-input js-panel__horizontal-input">
           <span class="panel__label-title">horizontal</span>
         </label>
       </form>
@@ -51,15 +51,15 @@ const template = `
       <div class="panel__item-title">Type:</div>
       <form class="panel__form panel__type-form">
         <label class="panel__label">
-          <input type="radio" name="type" class="panel__input panel__from-start-input js-panel__from-start-input">
+          <input type="radio" name="type" data-option-type="type" value="from-start" class="panel__input panel__from-start-input js-panel__from-start-input">
           <span class="panel__label-title">from-start</span>
         </label>
         <label class="panel__label">
-          <input type="radio" name="type" class="panel__input panel__from-end-input js-panel__from-end-input">
+          <input type="radio" name="type" data-option-type="type" value="from-end" class="panel__input panel__from-end-input js-panel__from-end-input">
           <span class="panel__label-title">from-end</span>
         </label>
         <label class="panel__label">
-          <input type="radio" name="type" class="panel__input panel__range-input js-panel__range-input">
+          <input type="radio" name="type" data-option-type="type" value="range" class="panel__input panel__range-input js-panel__range-input">
           <span class="panel__label-title">range</span>
         </label>
       </form>
@@ -68,15 +68,15 @@ const template = `
       <div class="panel__item-title">Other settings:</div>
       <div class="panel__form">
         <label class="panel__label">
-          <input type="checkbox" class="panel__input panel__range-input js-panel__withRange-input">
+          <input type="checkbox" data-option-type="withRange" class="panel__input panel__range-input js-panel__withRange-input">
           <span class="panel__label-title">withRange</span>
         </label>
         <label class="panel__label">
-          <input type="checkbox" class="panel__input panel__thumb-input js-panel__withThumb-input">
+          <input type="checkbox" data-option-type="withThumb" class="panel__input panel__thumb-input js-panel__withThumb-input">
           <span class="panel__label-title">withThumb</span>
         </label>
         <label class="panel__label">
-          <input type="checkbox" class="panel__input panel__scale-input js-panel__withScale-input">
+          <input type="checkbox" data-option-type="withScale" class="panel__input panel__scale-input js-panel__withScale-input">
           <span class="panel__label-title">withScale</span>
         </label>
       </div>
