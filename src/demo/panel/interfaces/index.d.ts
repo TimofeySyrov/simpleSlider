e2a2767b3b326c46ea42e287994934a928eb8348
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-09-23 18:47:07 +0300

    Перенос интерфейсов панели в директорию utils

--- src/demo/panel/interfaces/index.d.ts
+++
@@ -1,8 +0,0 @@
-import SimpleSlider from "../../../slider/simpleSlider";
-import SimpleSliderPanel from "../SimpleSliderPanel";
-
-declare global {
-  interface JQuery {
-    simpleSliderPanel(slider: SimpleSlider): SimpleSliderPanel;
-  }
-}
---
+++ src/demo/panel/utils/interfaces/IJQueryPanel.d.ts
@@ -0,0 +1,8 @@
+import SimpleSlider from "../../../../slider/simpleSlider";
+import SimpleSliderPanel from "../../SimpleSliderPanel";
+
+declare global {
+  interface JQuery {
+    simpleSliderPanel(slider: SimpleSlider): SimpleSliderPanel;
+  }
+}
