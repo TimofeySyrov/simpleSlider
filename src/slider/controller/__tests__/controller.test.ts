d9d5d941a814da881a9d2e413a2dbdb5e1547160
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-12-02 16:58:51 +0300

    Рефакторинг controller теста

--- src/slider/controller/__tests__/controller.test.ts
+++ src/slider/controller/__tests__/controller.test.ts
@@ -2,38 +2,26 @@
  * @jest-environment jsdom
  */
 import IUserOptions from '../../utils/interfaces/IUserOptions';
-import ICorrectOptions from '../../utils/interfaces/ICorrectOptions';
 import { TCurrentValue, TUpdateCurrentValue } from '../../utils/types/namespace';
 import defaultModelOptions from '../../utils/defaultModelOptions';
 import Controller from '../Controller';
 
 describe('Controller:', () => {
+  const domParent = document.createElement('div');
+  const controller = new Controller(domParent, defaultModelOptions);
+
+  beforeEach(() => {
+    controller.updateOptions(defaultModelOptions);
+  });
+
   describe('options:', () => {
     test('должен вернуть опции слайдера', () => {
-      const newOptions: ICorrectOptions = {
-        min: -10,
-        max: 0,
-        currentValue: { from: -3, to: -7 },
-        step: 0.1,
-        type: 'range',
-        orientation: 'vertical',
-        withThumb: true,
-        withRange: true,
-        withScale: true,
-      };
-
-      const mockParent = document.createElement('div');
-      const controller = new Controller(mockParent, newOptions);
-
-      expect(controller.options).toStrictEqual(newOptions);
+      expect(controller.options).toStrictEqual(defaultModelOptions);
     });
   });
 
   describe('events:', () => {
     test('должен вернуть эвенты слайдера', () => {
-      const mockParent = document.createElement('div');
-      const controller = new Controller(mockParent, defaultModelOptions);
-
       expect(controller.events.currentValueChanged).toBeTruthy();
       expect(controller.events.modelOptionsChanged).toBeTruthy();
       expect(controller.events.onSlide).toBeTruthy();
@@ -47,9 +35,6 @@ describe('Controller:', () => {
         withScale: false,
       };
 
-      const mockParent = document.createElement('div');
-      const controller = new Controller(mockParent, defaultModelOptions);
-
       controller.updateOptions(newOptions);
 
       expect(controller.options).toStrictEqual({ ...defaultModelOptions, ...newOptions });
@@ -58,26 +43,14 @@ describe('Controller:', () => {
 
   describe('updateCurrentValue:', () => {
     test('должен обновлять текущее значение слайдера', () => {
-      const newOptions: ICorrectOptions = { ...defaultModelOptions, ...{ type: 'range' } };
+      const newOptions: IUserOptions = { type: 'range' };
+      const newFrom: TUpdateCurrentValue = { option: 'from', value: 57 };
+      const newTo: TUpdateCurrentValue = { option: 'to', value: 97 };
 
-      const newFrom: TUpdateCurrentValue = {
-        option: 'from',
-        value: 57,
-      };
-
-      const newTo: TUpdateCurrentValue = {
-        option: 'to',
-        value: 97,
-      };
-
-      const mockParent = document.createElement('div');
-      const controller = new Controller(mockParent, newOptions);
-
-      // Так как в newOptions не указали to, то сначала обновляем его
+      controller.updateOptions(newOptions);
       controller.updateCurrentValue(newTo);
       controller.updateCurrentValue(newFrom);
-
-      const result: TCurrentValue = { from: 57, to: 97 };
+      const result: TCurrentValue = { from: newFrom.value, to: newTo.value };
 
       expect(controller.options.currentValue).toStrictEqual(result);
     });
